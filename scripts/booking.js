bf6ec11e1c8106ca68b9044062c4937123d080bd
matfren <153774963+matfren@users.noreply.github.com>
2023-12-13 22:57:02 -0700

    Add files via upload

--- scripts/booking.js
+++ scripts/booking.js
@@ -2,24 +2,25 @@
 // useful variables might be: the cost per day, the number of days selected, and elements on the screen that will be clicked or will need to be modified. 
 // Do any of these variables need to be initialized when the page is loaded? 
 // When do they need to be reset or updated?
-
-
-
+const dayButtons = document.getElementsByClassName("day-selector");
+const clearColor = document.getElementById("clear-button");
 
 /********* colour change days of week *********/
 // when the day buttons are clicked, we will apply the "clicked" class to that element, and update any other relevant variables. Then, we can recalculate the total cost.
 // added challenge: don't update the dayCounter if the same day is clicked more than once. hint: .classList.contains() might be helpful here!
 
 
+function changeColor() {
+    dayButtons.backgroundcolor.innerHTML = "#E5AF42";
+}
+
+dayButtons.addEventListener("click", changeColor);
 
 
 
 /********* clear days *********/
 // when the clear-button is clicked, the "clicked" class is removed from all days, any other relevant variables are reset, and the calculated cost is set to 0.
-
-
-
-
+clearColor.reset();
 
 
 /********* change rate *********/
@@ -27,11 +28,7 @@
 
 
 
-
 // when the full-day button is clicked, the daily rate is set back to $35, the clicked class is added to "full" and removed from "half", and the total cost is recalculated.
-
-
-
 
 
 /********* calculate *********/
--- scripts/contact.js
+++ scripts/contact.js
@@ -1,4 +1,10 @@
 // when the "submit-button" is clicked, the contents of the contact-page are replaced with a single <p> element that reads "Thank you for your message" in size 24 font.
+const submitButton = document.getElementById("submit-button");
+
+submitButton.onpointerover = thankYou;
+
+function thankYou() {
+    submitButton.innerHTML = "Thank you for your message";
+}
 
 // hint: you can change the style of an element by modifying the value of that element's .style.fontSize, or by updating its .classList.
-
